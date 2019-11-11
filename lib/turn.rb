c13981093194f52aa714ba49a13b03b6f3261dce
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-11-11 03:15:13 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,15 @@
+def valid_move?(board, index)
+  if position_taken?(board, index) == false && index >= 0 && index <= 8
+    return true
+  else
+    return false
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
