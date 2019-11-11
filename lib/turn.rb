2a3c7979da845ee75a7dbf84616487a2f5e3a4a8
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-11-11 03:34:28 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,7 +35,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  if position_taken?(board, index) == true
+  if valid_move?(board, index) == true
     move(board, index)
   else
     puts "invalid entry. Please enter 1-9:"
